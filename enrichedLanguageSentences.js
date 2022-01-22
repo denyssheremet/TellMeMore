fadedc7beb9caf901544b68d13c8a86a1e3a600a
Denys <d.o.sheremet@student.tudelft.nl>
2022-01-22 14:20:24 +0100

    add other files

---
+++ enrichedLanguageSentences.js
@@ -0,0 +1,242 @@
+
+
+var enrichedLanguageSentences = {
+    sentences: {
+        visual: {
+            examples:
+                `
+        I saw the apple.
+        The apple lay there.
+        The ball was huge. 
+        I saw her waving at me.
+        The skies were orange.
+        The light was really bright.
+        I looked at a person waving at me from a distance.
+        When we made eye contact, it felt like we were the only people in the room.
+        Everything looked fuzzy that day. 
+        I saw her sitting to the left of me.
+        Visual
+        • I saw him
+        • He saw me
+        • The tomatoes in the garden were large
+        • The Opera House was smaller than you would think
+        • The pen was too small for his hands
+        • The alien life forms waving at him were tiny
+        • The old oak tree towered over the little shed
+        • The young woman sitting opposite him stared straight ahead for the entire journey
+        • The girl's eyes darted around the room, not knowing where to rest
+        • The old man slowly got up from where he had been sitting
+        • The clouds drifted by as if they had somewhere urgent to be
+        • The tiger jumped out of the bushes with incredible speed
+        • His breathing was fast and uncontrolled
+        • It was a grey day
+        • The theme park was ever so colourful
+        • The film was in black and white
+        • The lady's neck turned red
+        • The rainbow faded away, leaving the sky greyer than before
+        • The train was brightly lit by the cold light of the TL-lights
+        • The hedgehog stared into the bright headlights
+        • Only the weak light of the moon gave the forest form
+        • The light was fading and it became more difficult to see her face
+        • The hotel lobby was dimly lit
+        • From the mountain the village looked so far
+        • She sat down next to her, barely leaving space between them
+        • The seats were only two rows from the emergency exit
+        • Had there been no glass, she could have stretched her right arm out and stroked his cheek
+        • He turned to say something, but she was already too far to hear him
+        • He couldn't focus on the lecture; he kept looking for something else to look at
+        • The streets seemed to stare at him attentively
+        • The world lost its focus
+        • His eyes were focused solely on the chair in front of him
+        • When he tried to recall his own face, he saw only a blur
+        • The rain on the window blended the lights of the city together into a neon milkshake
+        • He couldn't make out his grandmother's face until she stepped onto the platform, out of the fog
+        • The alcohol made his sight fuzzy
+        • The cocaine cleared her mind
+        • On the floor lay a piece of paper
+        • A train came in from the left
+        • Her right hand now held a knife
+        • The student looked up and locked eyes with the teacher
+        • Down at the gate, a man got out of a car
+       `
+        },
+
+        audio: {
+            examples:
+                `
+        Her voice echoed from all walls of the room.
+        It was the end of the day and my voice had become completely monotone.
+        To make sure everyone understood me, I spoke very slowly.
+        When I heard her voice, I immediately noticed how high pitched her voice was. 
+        And then I told her the secret in a very soft voice. 
+        It was difficult to hear what she was saying.
+        He was probably not aware of how loudly he was speaking.
+        He spoke with long pauses that gave a clear rhythm to his speech.
+        • The screaming seemed to come from everywhere
+        • Both his ears were now filled up with one parent yelling into them
+        • He couldn't make out where the sound was coming from
+        • The sound travelled from left to right
+        • The bass solo filled up the whole room
+        • In one flat tone, he dismembered the entire argument
+        • She told the story in an engaging way
+        • The old man recounted the story with the energy of his once eighteen year-old self
+        • The voices of the market were heavily engaged
+        • The conversation lacked tonality
+        • He spoke as if he was always in a hurry to get to the next sentence
+        • Their slow words echoed through the room in an even slower fashion
+        • The kids rushed to tell their story
+        • He spoke with the calm speed of someone who had nowhere to be
+        • The old cat let out a lengthy yawn
+        • She squealed from excitement
+        • He lowered his tone
+        • Martin let out a grumble
+        • The dog's high pitched begging for attention left the woman annoyed
+        • His low morning voice had barely discernible words
+        • He addressed his students with a soft voice
+        • The alarm was not loud or soft but intense
+        • His voices sounded stern, packed with urgency
+        • Whenever they called, his tone would soften
+        • Her words were neither here nor there, but they had the intensity of a speech
+        • You spoke clearly
+        • I could barely hear her fuzzy mumbling
+        • The boat's rumbling lacked the clarity he was used to from his car
+        • I heard him clearly
+        • The voice through the speakers was fuzzy
+        • He yelled his words above everyone else
+        • The shop was silent
+        • His brain was being loud
+        • He couldn't help speaking less and less softly in the library
+        • The volume of the croaking frogs was unbearable
+        • The rap had a regular rhythm
+        • The song was played slightly off-beat
+        • He couldn't find a rhythm in his speech
+        • All she heard was the cadence of his dangling keys
+        • Your presentation was disturbed by the scream in the distance and you never found your rhythm again
+    `
+        },
+
+        kinesthetic: {
+            examples:
+                `
+        I felt something in my chest.
+        My head was buzzing.
+        The meeting moved on slowly. 
+        I tapped her shoulder lightly.
+        It felt like trying to put a round peg in a square hole.
+        This conversation felt small in comparison to the one we had last week.
+        The temperature in the room was rising.
+        It feels like in this meeting we moved the project in the right direction.
+        During the whole presentation, I felt shaky. 
+        It was a sweet conversation.
+        It smelled as if we were in a bakery.
+        • I felt it in my stomach
+        • My head hurt
+        • My fingers started tingling
+        • I felt it in my bones
+        • It sent shivers down my spine
+        • His heart felt still
+        • I felt waves of euphoria come over me
+        • His head was pulsing
+        • The blood in his veins was pulsing harder and harder
+        • As his breathing calmed down, his mind became still again
+        • She kept shifting in her chair
+        • The train came to a stop again
+        • The pendulum swinged as it had always done
+        • The chair moved out of nowhere
+        • Major Tom flew through space for infinity
+        • I felt a pressure on my chest
+        • I felt no pressure on my chest
+        • If I failed this test, I couldn't come home
+        • I drank another so they would like me
+        • She touched his hand every so lightly
+        • It smelled square
+        • It felt very round
+        • I felt like a square because I would not drink
+        • It felt like sharp needles were being stabbed into me
+        • Our love was like a parallelepipedon
+        • The wine had a well-rounded taste
+        • It had a grand smell
+        • Even his kiss tasted small
+        • The storm brewing inside of him was still small
+        • A tiny hint of rosemary could be picked up by a trained nose
+        • Rose felt a great anger rising in her
+        • The hot coffee burned her tongue
+        • The scent of warm toast filled up the room
+        • Her touch felt colder than it had before
+        • He was burning up
+        • Doctor Richards came home that night to a warm fireplace
+        • Her embrace was a warm one
+        • The cold slowly rose from my feet to my chest
+        • The smell seemed to follow the cyclist
+        • The spiciness quickly found its way down his throat
+        • She traced his arm up to his shoulder
+        • His heart sank into his stomach
+        • The joy never stayed long
+        • It was a lasting sadness
+        • The smell came and went and came again
+        • The soul of the city had steadily increased in seriousness over those few years
+        • The breeze was strong and then soft again, with the only constant being it never being constant
+        • He felt bittersweet
+        • The strawberries had a sweetness he had long forgotten existed
+        • The essence in the air was a sour one
+        • Her cakes tasted like a summer breeze
+        • Kate welcomed the salty wind of the North sea
+    `
+        }
+    }
+};
+
+var modalityNeutralSentences =
+    `
+I walked to the supermarket to get the tomatoes
+He waited for a long time before the deer came in to call him
+You went to the flower store and left empty handed
+He walked towards her, smiled and kissed her hand
+He told them to fuck off and stepped back on his bike
+Tim stabbed Tom and drove him to the hospital
+They yelled at the dog, who had just barked at the little girl
+The square was empty; only a cat walked across it
+She hit the shark with his surfboard while screaming for help
+The man would climb mountains and dance naked on top of them
+He explained the problem to the kid, emphasising that it could not be solved. 
+He regretting drinking a whole bottle of rum and declaring his love to the maiden the night before
+The sun set on the village as the market closed up
+With the storm brewing, she tried to make it to the harbour in time
+He slammed the book on the desk and looked at the class
+She laughed and looked away
+They went to the club and danced their feet off for three hours
+The old man got up from his chair and fetched a bowl of chips
+He grabbed the kid and swam to the surface
+The building was about to collapse and one woman was still in there scrambling for her stuff
+He walked out of the door, closed it behind him and started to cry
+This was his first time in a baseball match and he missed his first catch, dropping the ball on the floor
+The arrow missed the mark and landed in a passing chicken
+I packed my bag and quickly left the dormitory
+`
+
+var modalities = {
+    visual: ["association", "size", "motion", "colour", "brightness", "distance", "focus", "clarity", "location"],
+    audio: ["location", "tonality", "tempo", "pitch", "intensity", "clarity", "volume", "rhythm"],
+    kinesthetic: ["location", "vibration", "movement", "pressure", "shape", "size", "temperature", "direction", "steadiness", "taste", "smell"],
+}
+
+var modalities1 = {
+    Visual: ["Association (associated or dissociated)", "Size (large or small)", "Motion (still or moving, slideshow or movie)", "Colour (colour or black and white)", "Brightness (bright or dark)", "Distance (near or far)", "Focus (focused or unfocused)", "Clarity (clear or fuzzy)", "Location (top, bottom, left, or right)"],
+    Audio: ["Location (mono, stereo, surround)", "Tonality (flat or engaging)", "Tempo/Pace (slow or fast)", "Pitch (high or low)", "Intensity (intense or soft)", " Clarity (clear or fuzzy)", "Volume (loud or soft)", "Rhythm (regular or irregular)"],
+    Kinesthetic: ["Location (Where do you feel it?)", "Vibration (Is it still or pulsing?)", "Movement (Is it still or moving?)", "Pressure (Is there any pressure? If so, is it light or intense?)", "Shape (What shape is it?)", "Size (How big is it?)", "Temperature (Does it have a temperature?)", "Direction (Does it have a direction? If so describe it.)", "Steadiness (Is it steady or intermittent?)", "Taste / Smell (Sweet, Sour, Salt, Bitter.)", "Smell (Aroma, Fragrance, Essence, Pungence.)"],
+}
+
+
+// turn string of sentences in modalities into arrrays
+// for (var [key, value] of Object.entries(enrichedLanguageSentences)) {
+//     enrichedLanguageSentences[key] = value.split("\n").slice(1, -1);
+//     for (let i = 0; i < enrichedLanguageSentences[key].length; i++) {
+//         enrichedLanguageSentences[key][i] = enrichedLanguageSentences[key][i].replace('•', '').trim();
+//     }
+// };
+
+// turn string of neutral sentences into arrays
+modalityNeutralSentences = modalityNeutralSentences.split("\n").slice(1, -1);
+for (let i = 0; i < modalityNeutralSentences.length; i++) {
+    modalityNeutralSentences[i] = modalityNeutralSentences[i].trim();
+}
---
+++ reframing.js
@@ -0,0 +1,166 @@
+
+
+
+var frames = [
+    "Deframing - Specificity", "Deframing - Sequence",
+    "Content Reframe", "Context Reframe", "Reflexive Reframe",
+    "Positive Prior Intention Framing", "Positive Prior Cause Reframing", "First Outcome Framing", "Outcome of Outcome Framing", "Eternity Framing",
+    "Counter Examples",
+    "Model of the World Framing", "Hierarchy of Criteria Reframe", "Allness Framing", "Necessity Framing", "Identity Framing", "Framing all other Abstractions",
+    "Unreality Frame", "Self and Other Frame", "Tonal Emphasis Frame", "Time Zones Frame", "Realization Frame", "Ecology Frame",
+    "Analogous Reframing", "Metaphor Reframing",
+    "Both And Framing", "Pseudo Word Framing", "Negation Framing", "As If Framing", "Systemic and Probability Framing", "Decision Framing"
+];
+
+var sleightOfMouth = [
+    "Intention",
+    "Redefine",
+    "Consequence",
+    "Chunk Down",
+    "Chunk Up",
+    "Analogy",
+    "Change Frame Size",
+    "Another Outcome",
+    "Model of the World",
+    "Reality Strategy",
+    "Counterexample",
+    "Hierarchy of Criteria",
+    "Apply to Self",
+    "Meta Frame",
+]
+
+var negativeBeliefs = [
+    "I'm a victim of my health.",
+    "I don't deserve to make the kind of money I want.",
+    "There is not enough time to do what is important.",
+    "Dating is difficult.",
+    "If you want something too bad, you can't have it.",
+    "I am ...",
+    "No pain no gain.",
+    "If you work hard, you will make a lot of money.",
+    "You raising your voice means you don't respect me.",
+    "Acting confidently is not easy.",
+    "If you study hard, you will get good grades. And it will make you worthy.",
+    "I can't do X because I've never done it before.",
+    "If I care about other people, they will take advantage of me",
+    "I am bad at dating",
+    "I am not confident",
+    "I can't have a good, strong attitude.",
+    "You're late, so that means you don't care about me.",
+    "Saying mean things means you're a bad person.",
+    "This house is too expensive, so I can't afford it.",
+    "I am bad at public speaking.",
+    "Cancer causes death.",
+    "I'm disorganized.",
+    "Cause and effect is real.",
+    "Making mistakes is bad.",
+    "Failing is painful.",
+
+    "Being rejected by a girl is painful.",
+    "You can't change your personality at quickly.",
+    "You have to always be nice to people.",
+    "It is bad to ask for what you want.",
+]
+
+
+var reframingDirection = ["Deframing", "Reframing", "Pre-Framing", "Post-Framing", "Counter-Framing", "Outframing", "Metaphorical Framing"];
+
+var miltonPatterns = [
+    "Unspecified Verb", "Nominalization", "Universal Quantifiers",
+    "Lost Performative", "Lack of Referential Index", "Unspecified Noun",
+    "Easifiers", "Open-Ended Suggestion", "Confirmation Questions", "Confirmation Words",
+    "Selective Restriction Violation", "Have you ever (discovered)...", "Why not...", "Consent, then Inverse",
+    "Why is it that some people see it, and most don't?", "I do the work for you", "Only/Last Option", "Simple Deletion", "Try...",
+    "Reading Minds", "Truisms & Comparative Deletions", "Modal Operators", "Embedded commands", "Extended Quotes", "Nested Loops/Quotes",
+    "Trance Words", "Anticipation Builders", "Pattern Interrupts", "Language Softeners", "Negative Formulation",
+    "Cause and Effect", "Complex Equivalence", "Presuppositions", "Double Binds", "Conversational Postulate", "Incorrect Grammar",
+    "Pacing & Leading", "Utilization", "Metaphors", "Analog", "Fractionation", "Future Pace", "Scope Ambiguity", "Punctuation Ambiguity"
+]
+
+var miltonPatterns1 = [
+    // "Cause - Effect", "Implied Causative", "Conjunction", "Mind Reading",
+    "Generalized Referential Index",  "Selectional Restriction Violation", "Unspecified Verb",
+    "Deletion", "Sentence Fragment", "Nominalization",
+    // "Phonological Ambiguity", "Syntactic Ambiguity", "Scope Ambiguity", "Punctuation Ambiguity",
+    // "Embedded Question", "Embedded Command", "Quotes",
+    // "Presupposition", "Conversational Postulate",
+]
+// Lost performative 
+
+var presuppositionEnvironments = [
+    "Proper Names",
+    "Pronouns",
+    "Definite Descriptions",
+    "Generic Noun Phrases",
+    "Some Quantifiers",
+    "Relative Clauses",
+    "Subordinate Clauses of Time",
+    "Cleft Sentences",
+    "Pseudo-Cleft Sentences",
+    "Stressed Sentences",
+    "Complex Adjectives",
+    "Ordinal Numerals",
+    "Comparatives",
+    "Comparative as",
+    "Repetitive Cue Words",
+    "Repetitive Verbs and Adverbs",
+    "Qualifiers",
+    "Change of Place Verbs",
+    "Change of Time Verbs",
+    "Change of State Verbs",
+    "Tactive Verbs and Adjectives",
+    "Commentary Adjectives and Adverbs",
+    "Counterfactual Conditional Clauses",
+    "Contrary to Expectation",
+    "Selectional Restrictions",
+    "Questions",
+    "Negative Questions",
+    "Rhetorical Questions",
+    "Spurious not",
+]
+
+var mainPresuppositions = [
+    "Subordinate Clauses of Time",
+    "Ordinal Numerals",
+    "Use of 'Or'",
+    "Awareness Predicates",
+    "Adverbs and Adjectives",
+    "Change of Time Verbs and Adverbs",
+    "Commentary Adjectives and Adverbs",
+]
+
+var importantSubModalities = [
+    "Location",
+    "Distance",
+    "Volume",
+    "Clarity",
+    "Brightness",
+    "Size",
+    "Movement",
+    "Perspective",
+    "Proportion",
+    "Pitch",
+    "Melody",
+    "Tonality",
+    "Tempo",
+    "Rhythm",
+    "Mono/Stereo",
+    "Temperature",
+    "Taste",
+    "Smell",
+    "Direction",
+    "Intensity",
+]
+
+var presuppositions = [
+    "In any system with human beings and machines, the system with the widest range of flexibility is going to be the controlling element.",
+    "You can change anybody's mind if you use enough behavioural flexibility.",
+    "You can change anybody's mind.",
+    "You have to have more belief in any human being than they have in themselves. They are stuck in their beliefs. You are able to help them.",
+    "The meaning of communication is the response you get.",
+    "I one person can do it, anybody can learn it.",
+    "You can only fail if you set your timeframe too short.",
+    "Challenge is fun. It's fun to look for the hard sell.",
+]
+
+
