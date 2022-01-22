6967a2424a6a356df20def9985ad809f4f7533b6
Denys <d.o.sheremet@student.tudelft.nl>
2022-01-22 14:29:02 +0100

    removed all except Meta Model Trainer

--- main.js
+++ main.js
@@ -392,208 +392,6 @@ function startMultipleChoiceTrainer(dict, title, buttons = true) {
 };
 
 
-function startEnrichedLanguageTrainer1() {
-    document.getElementById("title").innerHTML = "Enriched Language Trainer 1 (Beginner)";
-    document.getElementById("bottomDiv").style.display = "flex";
-    document.getElementById("bottomDiv").style.margin = "100px";
-    question = new Question()
-
-    mcd = new MultipleChoiceDict(enrichedLanguageSentences);
-
-    question.chooseNextQuestion();
-
-    // make answer buttons
-    for (let i = 0; i < mcd.getCategoryKeys().length; i++) {
-        let category = mcd.getCategoryKeys()[i];
-
-        for (let j = 0; j < mcd.getSubCategoryKeys(category).length; j++) {
-            let subCategory = mcd.getSubCategoryKeys(category)[j];
-            makeAnswerButton(subCategory, "answerButton answer:" + subCategory, "bottomDiv",
-                function () { answerChosen(subCategory); question.chooseNextQuestion(); })
-        }
-    }
-}
-
-function startEnrichedLanguageTrainer2() {
-    document.getElementById("title").innerHTML = "Enriched Language Trainer 2 (Advanced)";
-    question = new Question()
-    makeH3("", "topDiv", "submods");
-    makeTextArea("textarea", "bottomDiv");
-    autocomplete(document.getElementById("textarea"), basicNeeds, "bottomDiv");
-
-    getRandSubmodalities(3);
-    getRandSentence(modalityNeutralSentences);
-
-    let textarea = document.getElementById("textarea");
-
-    textarea.onkeydown = function () {
-        var key = event.keyCode || event.charCode;
-        if (key == 13) {
-            document.getElementById("textarea").value = "";
-            getRandSubmodalities(3);
-            getRandSentence(modalityNeutralSentences);
-        }
-    };
-};
-
-function startIntentionReframeTrainer1() {
-    document.getElementById("title").innerHTML = "Intention Reframe Trainer 1";
-
-    question = new Question()
-    getRandSentence(negativeBehaviours);
-
-    for (let i = 0; i < basicNeeds.length; i++) {
-        makeTextArea("textarea" + (i + 1), "bottomDiv", placeholder = basicNeeds[i], small = true);
-    }
-
-    document.getElementById("textarea6").onkeydown = function () {
-        var key = event.keyCode || event.charCode;
-        if (key == 13) {
-            for (let i = 0; i < basicNeeds.length; i++) {
-                document.getElementById("textarea" + (i + 1)).value = "";
-            }
-            getRandSentence(negativeBehaviours);
-        }
-    };
-};
-
-function startLogicalLevelsTrainer1() {
-    document.getElementById("title").innerHTML = "Logical Levels Trainer 1";
-
-    question = new Question()
-    getRandSentence(negativeBehaviours);
-
-    for (let i = 0; i < logicalLevels.length; i++) {
-        makeTextArea("textarea" + (i + 1), "bottomDiv", placeholder = logicalLevels[i], small = true);
-    }
-
-    document.getElementById("textarea6").onkeydown = function () {
-        var key = event.keyCode || event.charCode;
-        if (key == 13) {
-            for (let i = 0; i < logicalLevels.length; i++) {
-                document.getElementById("textarea" + (i + 1)).value = "";
-            }
-            getRandSentence(negativeBehaviours);
-        }
-    };
-}
-
-function startWritingTrainer1(fromList, title) {
-    let score = 0;
-    let amount = 4;
-    document.getElementById("title").innerHTML = title + score;
-    let dv = document.createElement("div");
-    dv.id = "innerDiv";
-    dv.style.margin = "50px";
-    document.getElementById("topDiv").appendChild(dv);
-
-    // question = new Question()
-    // makeH3("", "topDiv", "sentence");
-    // getRandSentence(fromList);
-    let rand = randFromList(fromList, amount);
-    for (let i = 0; i < amount; i++) {
-        makeH3(rand[i], "innerDiv");
-    }
-    makeTextArea("textarea", "bottomDiv");
-
-
-    document.getElementById("textarea").onkeydown = function () {
-        var key = event.keyCode || event.charCode;
-        if (key == 13) {
-            document.getElementById("textarea").value = "";
-            document.getElementById("innerDiv").innerHTML = "";
-            rand = randFromList(fromList, amount);
-            for (let i = 0; i < amount; i++) {
-                makeH3(rand[i], "innerDiv");
-            }
-            score += amount;
-            document.getElementById("title").innerHTML = title + score;
-        }
-    };
-}
-
-
-function startDoubleBindTrainer() {
-    document.getElementById("title").innerHTML = "Meta Program Trainer 2";
-
-    for (let i = 0; i < 3; i++) {
-        let dv = document.createElement("div");
-        dv.id = "doubleBindDiv" + i;
-        document.getElementById("topDiv").appendChild(dv);
-        makeTextArea("t1", "doubleBindDiv" + i);
-        makeTextArea("t2", "doubleBindDiv" + i);
-    }
-
-    makeTextArea("t3", "topDiv");
-}
-
-
-function startMetaProgramTrainer2() {
-    document.getElementById("title").innerHTML = "Meta Program Trainer 2";
-    let amountOfMetaPrograms = 5;
-
-    dropdown.clearContent();
-    dropdown.toggleVisibility(true);
-    question = new Question();
-
-    makeDropdownButton("Motivation", function () { activeMetaPrograms = "motivation"; dropdown.toggleOpen(); });
-    makeDropdownButton("Productivity", function () { activeMetaPrograms = "productivity"; dropdown.toggleOpen(); });
-    makeDropdownButton("All", function () { activeMetaPrograms = "all"; dropdown.toggleOpen(); });
-
-
-    // create div inside topDiv
-    var dv = document.createElement("div");
-    dv.id = "metaProgramsDiv";
-    document.getElementById("topDiv").appendChild(dv);
-
-    // create h3s in topDiv
-    for (let i = 0; i < amountOfMetaPrograms; i++) {
-        makeH3("a", "metaProgramsDiv", "mp:" + (i + 1));
-    }
-
-    // create textareas in bottomDiv
-    for (let i = 0; i < contexts.length; i++) {
-        makeTextArea("textarea" + (i + 1), "bottomDiv", placeholder = contexts[i], small = true);
-    }
-
-    // fill h3's with random meta programs
-    chooseNextMetaPrograms(amountOfMetaPrograms);
-
-
-    // on enter key: set new random meta programs and clear text areas
-    document.getElementById("textarea4").onkeydown = function () {
-        var key = event.keyCode || event.charCode;
-        if (key == 13) {
-
-            for (let i = 0; i < contexts.length; i++) {
-                document.getElementById("textarea" + (i + 1)).value = "";
-            }
-            chooseNextMetaPrograms(amountOfMetaPrograms);
-        }
-    };
-}
-
-function startMetaProgramTrainer4() {
-    mcd = new MultipleChoiceDict(metaPrograms);
-    console.log(mcd);
-    let subCat = [];
-    for (let i = 0; i < mcd.getCategoryKeys().length; i++) {
-        let cat = mcd.getCategoryKeys()[i];
-        subCat.push(mcd.getSubCategoryKeys(cat));
-    }
-    subCat = subCat.flat();
-    console.log(subCat);
-
-    makeTextArea("", "topDiv", "Context:", true);
-
-    for (let i = 0; i < subCat.length; i++) {
-        makeTextArea("", "topDiv", subCat[i], true);
-    }
-
-    makeTextArea("", "topDiv", "Plan / Language:");
-
-}
-
 // Clears index.html so another Trainer can be started.
 function clearIndex() {
     document.getElementById("topDiv").innerHTML = "";
@@ -609,16 +407,7 @@ function clearIndex() {
     }
 };
 
-function startSelfAdviceWriter1() {
-    makeH2("Suggestion Model", "topDiv");
-    // makeH2("Macho Test", "topDiv");
-    makeH2("Sell to Internals", "topDiv");
-    // makeH2("3-2-1 Away From Sandwich", "topDiv");
-    makeH2("Bad News Formula", "topDiv");
-    // makeH2("4Mat Opener", "topDiv");
-    makeTextArea("", "bottomDiv");
 
-}
 
 function showTrainings() {
     if (trainingDropdownOpen) {
@@ -629,61 +418,13 @@ function showTrainings() {
     trainingDropdownOpen = !trainingDropdownOpen;
 };
 
-function selectTraining(trainingCode) {
-    showTrainings();
-    clearIndex();
-    switch (trainingCode) {
-        case "MMT1": startMultipleChoiceTrainer(metaModelSentences, "Meta Model Trainer");
-            break;
-        case "ELT1": startEnrichedLanguageTrainer1();
-            break;
-        case "ELT2": startEnrichedLanguageTrainer2();
-            break;
-        case "IRT1": startIntentionReframeTrainer1();
-            break;
-        case "LLT1": startLogicalLevelsTrainer1();
-            break;
-        case "MPT1": startMultipleChoiceTrainer(metaProgramsMC, "Meta Program Trainer 1");
-            break;
-        case "MPT2": startMetaProgramTrainer2();
-            break;
-        case "MPT4": startMetaProgramTrainer4();
-            break;
-        case "RET1": startWritingTrainer1(sleightOfMouth, "Reframing Trainer: ");
-            break;
-        case "MIL1": startWritingTrainer1(miltonPatterns1, "Milton Patterns: ");
-            break;
-        case "PRE1": startWritingTrainer1(mainPresuppositions, "Presuppositions: ");
-            break;
-        case "SUB1": startWritingTrainer1(importantSubModalities, "Submodalities: ");
-            break;
-        case "SAW1": startSelfAdviceWriter1();
-            break;
-        case "DBT1": startDoubleBindTrainer();
-            break;
-    }
-}
 
 window.addEventListener("load", function () {
-
-    MultipleChoiceDict.formatDict(enrichedLanguageSentences);
-    MultipleChoiceDict.formatDict(metaModelSentences);
-    MultipleChoiceDict.formatDict(metaProgramsMC);
-
 
     clearIndex();
     dropdown = new Dropdown("dropdown", "selectionDropdown");
     question = new Question()
 
-    // startMetaModelTrainer1();
-    // startEnrichedLanguageTrainer1();
-    // startEnrichedLanguageTrainer2();
-    // startIntentionReframeTrainer1();
-    // startLogicalLevelsTrainer1();
-    // startMetaProgramTrainer2();
-    // startReframingTrainer1();
-
-    selectTraining("MMT1");
-    showTrainings();
+    startMultipleChoiceTrainer(metaModelSentences, "Meta Model Trainer");
 });
 
